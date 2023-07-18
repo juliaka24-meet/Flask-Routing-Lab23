94b30d6487ed96aeb2ca209a416da47d29e922d4
juliaka-meet <juliakagan109@gmail.com>
2023-07-18 14:03:39 +0300

    jubay
    its kinda ugly but it works

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -1,18 +1,19 @@
 from flask import Flask, redirect, request, render_template, url_for
 
 
-app = Flask(  # Create a flask app
-    __name__,
-    template_folder='templates',  # Name of html file folder
-    static_folder='static'  # Name of directory for static files
-)
+app = Flask( __name__, template_folder='templates', static_folder='static')
 
-# Your code should be below
+@app.route("/")
+def home_page():
+	return render_template("home.html")
 
+@app.route("/product")
+def product_page():
+	return render_template("product.html")
 
-
-
-# Your code should be above
+@app.route("/cart")
+def cart_page():
+	return render_template("cart.html")
 
 if __name__ == "__main__":  # Makes sure this is the main process
     app.run(debug=True)
